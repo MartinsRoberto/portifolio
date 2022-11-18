ea4a4836d5c5c320765d02cc008c5066f64ce1bb
MartinsRoberto <robertomartinsramm@gmail.com>
2022-11-18 01:22:34 -0300

    configurando menu

--- script.js
+++ script.js
@@ -12,13 +12,10 @@ navList.addEventListener('click', function(e){
 
 function typeWriter(elemento) {
   const textoArray = elemento.innerHTML.split('');
-  console.log(textoArray)
   elemento.innerHTML = '';
   textoArray.forEach((letra, i) => {
     setTimeout(() => elemento.innerHTML += letra, 100 * i);
   });
-
-  
 }
 
 
