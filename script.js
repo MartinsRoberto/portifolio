629c2a095fdf479f08d70a8e349c8094734c259b
MartinsRoberto <robertomartinsramm@gmail.com>
2022-11-18 00:53:37 -0300

    versao final

---
+++ script.js
@@ -0,0 +1,26 @@
+const btnMobile = document.querySelector('.btn-mobile')
+const navList = document.querySelector('.nav-list')
+
+btnMobile.addEventListener('click', ()=>{
+  navList.classList.toggle('active')
+})
+navList.addEventListener('click', function(e){
+  navList.classList.toggle('active')
+})
+
+
+
+function typeWriter(elemento) {
+  const textoArray = elemento.innerHTML.split('');
+  console.log(textoArray)
+  elemento.innerHTML = '';
+  textoArray.forEach((letra, i) => {
+    setTimeout(() => elemento.innerHTML += letra, 100 * i);
+  });
+
+  
+}
+
+
+const titulo = document.querySelector('.meu-nome');
+typeWriter(titulo);
